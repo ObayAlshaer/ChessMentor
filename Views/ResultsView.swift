79e07f5909060dbce4296337d580ac2f941b703e
Anas <anashammou2003@gmail.com>
2025-09-25 21:39:48 -0400

    fix camera navigation and add gallery feature

--- Views/ResultsView.swift
+++ Views/ResultsView.swift
@@ -1,25 +1,23 @@
-//
-//  SwiftUIView.swift
-//  chessMentor
-//
-//  Created by Justin Bushfield on 2025-03-28.
-//
-
 import SwiftUI
 
 struct ResultsView: View {
-    
     @ObservedObject var camera: CameraModel
     
     var body: some View {
-        Text("Results Displayed Here")
-        if let capturedPhoto = camera.capturedPhoto {
-            Image(uiImage: capturedPhoto)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .edgesIgnoringSafeArea(.all)
-        } else {
-            Text("Uhhh, idek how u are able to see this")
+        VStack {
+            Text("Results Displayed Here")
+            if let capturedPhoto = camera.capturedPhoto {
+                Image(uiImage: capturedPhoto)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .edgesIgnoringSafeArea(.all)
+            } else {
+                Text("Uhhh, idek how u are able to see this")
+            }
+        }
+        //When leaving results, clear state so Back shows live camera
+        .onDisappear {
+            camera.retakePicture()
         }
     }
 }
